714ab289510af704f30ef0feb6720f5e696c29f4
Kristian Nygaard Jensen <krny@tv2.dk>
2012-05-17 11:49:37 +0200

    Edit project name in GPL license

--- elisp/drupal-mode.el
+++ elisp/drupal-mode.el
@@ -2,18 +2,18 @@
 
 ;; This file is part of drupal-emacs.
 
-;; Foobar is free software: you can redistribute it and/or modify
+;; drupal-emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; Foobar is distributed in the hope that it will be useful,
+;; drupal-emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+;; along with drupal-emacs.  If not, see <http://www.gnu.org/licenses/>.
 ;;;###autoload
 (define-derived-mode drupal-mode php-mode "Drupal"
   "Major mode for DRUPAL coding.\n\n\\{drupal-mode-map}"
