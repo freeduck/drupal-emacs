b16740078c90148d888b101d76c4c0c3aee544c4
Kristian Nygaard jensen <krny@tv2.dk>
2013-10-04 10:19:19 +0200

    Bind icicle-bookmark-all-tags to C-c j

--- elisp/drupal-mode.el
+++ elisp/drupal-mode.el
@@ -45,16 +45,11 @@
   (let ((tag-file (concat project-root-dir "TAGS")))
     (visit-tags-table tag-file)))
 
-(defun compile-tags-action (project-root-dir, current-dir)
-  "compile etags for the current project"
-  (cd project-root-dir)
-  ;(start-file-process-shell-command "shell-update-tags" nil "rm TAGS;find -L .  -name \"*.php\" -o -name \"*.inc\" -o -name \"*.module\"|xargs ctags -ea --language-force=PHP")
-  (start-file-process-shell-command "shell-update-tags" nil "rm TAGS;find  .  -name \"*.php\" -o -name \"*.inc\" -o -name \"*.module\"|xargs ctags -ea --language-force=PHP")
-  (set-process-filter (get-process "shell-update-tags") 'refresh-tags-table)
-  (cd current-dir))
-
 (defun compile-tags ()
   "compile etags for the current project"
   (interactive)
   (if (boundp 'project-root-dir)
     (compile-tags-action project-root-dir (file-name-directory  buffer-file-name))))
+
+(global-set-key (kbd "C-c j") 'icicle-bookmark-all-tags)
+
